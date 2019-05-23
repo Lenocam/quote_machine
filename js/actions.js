94828023ba433bbfeb9e847787ea9ec4518ed22b
Reed MacConnell <reed.macconnell@gmail.com>
2019-05-23 11:22:11 -0400

    changed source for quotes to a new source page bc the old one stopped working

--- js/actions.js
+++ js/actions.js
@@ -4,7 +4,8 @@ $(document).ready(function() {
   //jquery triggered on button click
   $("#getMessage").on("click", function() {
     //grabs json from api this is the api
-    $.getJSON("https://talaikis.com/api/quotes/random/", function(data) {
+    $.getJSON('https://gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/quotes.json', function(data) {
+      console.log(data);
       //declare html variable to put json on to in html formatted way
       var html = "";
       //use key and value from json object format to html add to html variable
@@ -15,14 +16,16 @@ $(document).ready(function() {
           html += "<div><br>";
         })
         */
-      var currentQuote = data.quote;
-      var currentAuthor = data.author;
-      var currentCat = data.cat;
+
+      var dataLength = data.length;
+      var selectedQuoteIndex = Math.floor(Math.random() * dataLength-1)+1;
+      var selectedQuote = data[selectedQuoteIndex];
+      var currentQuote = selectedQuote.quote;
+      var currentAuthor = selectedQuote.author;
 
       html += "<div class=quoteBox>";
       html += "<div class=quote>" + currentQuote + "</div><br>";
       html += "<div class=quoteAuthor>" + currentAuthor + "</div><br>";
-      html += "<div class=quoteCat><strong>Category:</strong> " + currentCat + "</div><br>";
       html += "</div><br>";
       //tell html var where and how to opened using html jsquery method
 
@@ -38,5 +41,5 @@ $(document).ready(function() {
 
     var tweetLink = "https://twitter.com/intent/tweet?text=" + encodeURIComponent(textToTweet);
     window.open(tweetLink, "tweet", "height=300,width=550,resizable=0");
-  })
+  });
 });
